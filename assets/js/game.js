fb391be59c39442c6714043ff077ee0a203e1082
tylers22 <tylersharp1@gmail.com>
2021-08-29 12:09:17 -0700

    basic if statements 3.1.7

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,41 @@
 var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log("This logs a string, good for leaving yourself a message");
-// this will do math and log 20
-console.log(10 + 10);
-// what is this?
-console.log("Our robot's name is " + playerName);
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
 
 // this creates a function named "fight"
 
-function fight() {
-    window.alert("the fight has begun!");
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators");
+
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+
+    // check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died! ");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+
+    playerHealth = playerHealth - enemyAttack;
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+
+    //check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + "still has " + playerHealth + " health left.");
+    }
 }
 
-// fight();
+fight();
